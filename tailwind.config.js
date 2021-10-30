bd58120c10ff247be130e8f31c58e860d56634f1
Chris Bongers <chrisbongers@gmail.com>
2021-10-30 10:57:04 +0200

    Start on mobile base layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,30 @@
 module.exports = {
+  theme: {
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      dark: '#061019',
+    }),
+    textColor: (theme) => ({
+      ...theme('colors'),
+      dark: '#061019',
+    }),
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.blue.800'),
+
+            // ...
+          },
+        },
+      }),
+    },
+  },
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+  plugins: [
+    require('@tailwindcss/typography')({
+      modifiers: [],
+    }),
+  ],
 };

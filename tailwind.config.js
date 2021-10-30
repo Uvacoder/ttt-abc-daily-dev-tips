eebd011c39f6670d7eb36394e37488c26e960bf5
Chris Bongers <chrisbongers@gmail.com>
2021-10-30 18:33:20 +0200

    Some more changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,23 @@
 module.exports = {
   theme: {
-    backgroundColor: (theme) => ({
-      ...theme('colors'),
-      dark: '#061019',
-    }),
-    textColor: (theme) => ({
-      ...theme('colors'),
-      dark: '#061019',
-    }),
     extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.blue.800'),
-
-            // ...
-          },
+      colors: {
+        barbie: {
+          light: '#f16cb6',
+          dark: '#a73a75',
         },
-      }),
+        ken: {
+          light: '#55e3f2',
+          dark: '#439ca6',
+        },
+        flash: {
+          light: '#f2e585',
+        },
+        dark: '#061019',
+      },
     },
   },
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
-  plugins: [
-    require('@tailwindcss/typography')({
-      modifiers: [],
-    }),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 };

1f5f9dfa4a39bbde581c34856bf4416fe188d15a
Chris Bongers <chrisbongers@gmail.com>
2022-02-22 19:56:51 +0200

    Fix SEO stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
+import autolinkHeadings from "remark-autolink-headings";
+import astroRemark from '@astrojs/markdown-remark';
+
 export default {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: 'https://daily-dev-tips.com/',
+    site: "https://daily-dev-tips.com/",
     sitemap: true, // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
-    tailwindConfig: './tailwind.config.js',
+    tailwindConfig: "./tailwind.config.js",
   },
   renderers: [],
 };

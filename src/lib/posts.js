f48a7f46f76e5b5cc9b56a81f4bff0b2451df1b6
Chris Bongers <chrisbongers@gmail.com>
2022-03-07 21:33:07 +0200

    Testing new version build

---
+++ src/lib/posts.js
@@ -0,0 +1,22 @@
+async function load() {
+  const fetchedPosts = await import.meta.glob("../pages/posts/*.md");
+  const mappedPosts = await Promise.all(
+    Object.keys(fetchedPosts).map((key) => {
+      const post = fetchedPosts[key];
+      const url = key.replace("../pages/", "/").replace(".md", "/");
+      return post().then((p) => {
+        return { ...p.frontmatter, url };
+      });
+    })
+  );
+
+  return mappedPosts;
+}
+
+let _posts;
+
+export async function getAllPosts() {
+  _posts = _posts || (await load());
+
+  return await _posts;
+}
